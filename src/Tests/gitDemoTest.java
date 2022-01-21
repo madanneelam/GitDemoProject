35c88937f7f58b8e7141c68537fe5249fd067fab
anilmadan <anilmadan@192.168.1.23>
2022-01-20 20:53:37 -0800

    First commit....

---
+++ src/Tests/gitDemoTest.java
@@ -0,0 +1,14 @@
+package Tests;
+
+public class gitDemoTest
+{
+ public static void main(String[] args)
+ {
+	 String hungry = "yes";
+	 
+	 if(hungry=="yes")
+		 System.out.println("eat something.....");
+	 else
+		 System.out.println("do some work.....");
+ }
+}
