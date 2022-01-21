79df8bf89f6907056888d9e379fdc86670ab99d5
nelumadan <nelu_madan@yahoo.com>
2022-01-20 22:06:16 -0800

    commiting test2 to branch_addTest2

---
+++ src/Tests/gitDemoTest2.java
@@ -0,0 +1,14 @@
+package Tests;
+
+public class gitDemoTest2
+{
+ public static void main(String[] args)
+ {
+	 String hungry = "yes";
+	 
+	 if(hungry=="yes")
+		 System.out.println("I am hungry....");
+	 else
+		 System.out.println("I am not hungry.....");
+ }
+}
